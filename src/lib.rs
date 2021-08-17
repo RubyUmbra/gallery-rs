6cd1e1aac1ac45898c4a1109281f6f65034765e5
RubyUmbra <rubyumbra@gmail.com>
2021-08-17 20:22:31 +0300

    Extract main to lib

---
+++ src/lib.rs
@@ -0,0 +1,68 @@
+use crate::image_storage::ImageStorage;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::path::{Path, PathBuf};
+use std::{env, fs};
+
+mod gui;
+mod image_storage;
+
+pub fn run() -> Result<(), String> {
+    let args: Vec<_> = env::args().collect();
+
+    let path = if args.len() < 2 {
+        env::current_dir().map_err(|e| e.to_string())?
+    } else {
+        Path::new(&args[1]).to_path_buf()
+    };
+
+    run_internal(path.as_path())
+}
+
+fn run_internal(path: &Path) -> Result<(), String> {
+    let mut storage: ImageStorage = ImageStorage::new(path)?;
+
+    let mut del = PathBuf::from(path);
+    del.push("del");
+    fs::create_dir_all(del.as_path()).map_err(|e| e.to_string())?;
+
+    let mut gui_context = gui::GuiContext::new()?;
+
+    gui_context.render_pic(storage.get().as_path())?;
+
+    'mainloop: loop {
+        for event in gui_context.sdl_context.event_pump()?.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Option::Some(Keycode::Escape),
+                    ..
+                }
+                | Event::KeyDown {
+                    keycode: Option::Some(Keycode::Q),
+                    ..
+                } => break 'mainloop,
+                Event::KeyDown {
+                    keycode: Option::Some(Keycode::Right),
+                    ..
+                } => storage.next(),
+                Event::KeyDown {
+                    keycode: Option::Some(Keycode::Left),
+                    ..
+                } => storage.prev(),
+                Event::KeyDown {
+                    keycode: Option::Some(Keycode::Space),
+                    ..
+                } => storage.mv(del.as_path()),
+                _ => {}
+            }
+            if storage.is_empty() {
+                return Err("No images".to_string());
+            }
+            gui_context.render_pic(storage.get().as_path())?;
+        }
+    }
+
+    Ok(())
+}
